1c8f6f7105fc16efd3308b5100c425a855e2e886
Pedro Cruz <pecruz06@gmail.com>
2026-05-25 11:06:51 -0300

    build(release): apply firebase-crashlytics gradle plugin

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -7,6 +7,7 @@ plugins {
     alias(libs.plugins.compose.multiplatform)
     alias(libs.plugins.compose.compiler)
     alias(libs.plugins.google.services)
+    alias(libs.plugins.firebase.crashlytics)
 }
 
 kotlin {
